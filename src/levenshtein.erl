a2142f2014af24e070ff1689ae95e3a75a3401a8
Chavez <nailsbrokeandfell@gmail.com>
2013-03-30 08:19:42 -0700

    Fix failing tests.

--- src/levenshtein.erl
+++ src/levenshtein.erl
@@ -1,25 +1,23 @@
 -module (levenshtein).
 -export ([distance/2]).
 
-distance(Str1, Str2) ->
-  {Length, _} = dist(Str1, Str2, dict:new()),
-  Length.
+distance(String1, String2) ->
+    {Length, _} = dist(String1, String2, dict:new()),
+    Length.
 
-dist([]=S1, S2, Cache) ->
-  L2 = length(S2),
-  {L2, dict:store({S1, S2}, L2, Cache)};
-
-dist(S1, []=S2, Cache) ->
-  L1 = length(S1),
-  {L1, dict:store({S1, S2}, L1, Cache)};
-
-dist([_SS1|S1]=Str1, [_SS2|S2]=Str2, Cache) ->
-  case dict:is_key({Str1, Str2}, Cache) of
-    true -> { dict:fetch({Str1, Str2}, Cache), Cache};
-    false ->
-      {L1, C1} = dist(Str1, S2, Cache),
-      {L2, C2} = dist(S1, Str2, C1),
-      {L3, C3} = dist(S1, S2, C2),
-      Len = 1 + lists:min([L1, L2, L3]),
-      {Len, dict:store({Str1, Str2}, Len, C3)}
-  end.
+dist([]=String1, String2, Cache) ->
+    {length(String2), dict:store({String1, String2}, length(String2), Cache)};
+dist(String1, []=String2, Cache) ->
+    {length(String1), dict:store({String1, String2}, length(String1), Cache)};
+dist([X|String1], [X|String2], Cache) ->
+    dist(String1, String2, Cache);
+dist([_|STail1]=String1, [_|Stail2]=String2, Cache) ->
+    case dict:is_key({String1, String2}, Cache) of
+        true -> {dict:fetch({String1, String2}, Cache), Cache};
+        false ->
+            {Len1, Cache1} = dist(String1, Stail2, Cache),
+            {Len2, Cache2} = dist(STail1, String2, Cache1),
+            {Len3, Cache3} = dist(STail1, Stail2, Cache2),
+            Len = 1+lists:min([Len1, Len2, Len3]),
+            {Len, dict:store({String1, String2}, Len, Cache3)}
+    end.
